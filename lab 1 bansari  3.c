ba5ae6e1073a9983b945f4f3ccdd1702b168d869
bansari006 <bansarigajera10@gmail.com>
2025-09-12 10:32:41 +0530

    Add files via upload

---
+++ lab 1 bansari  3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b ,product;
+    printf ("enter two number");
+    scanf("%d,%d" , &a,&b);
+    product=a*b;
+    printf("product= %d\n",sum0)
+    return 0 ;
+
+}
+

5f05ec56e6d41937e8f528049dface8110d09e6a
bansari006 <bansarigajera10@gmail.com>
2025-09-12 09:34:37 +0530

    Add files via upload

---
+++ lab 1 bansari 100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b ,sum;
+    printf ("enter two number");
+    scanf("%d,%d" , &a,&b);
+    sum=a+b;
+    printf("sum + %d\n",sum0)
+    return 0 ;
+
+}
+

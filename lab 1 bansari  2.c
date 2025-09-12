14bd5bd2ceec007abde080c124c73b36420212cf
bansari006 <bansarigajera10@gmail.com>
2025-09-12 10:14:17 +0530

    Add files via upload

---
+++ lab 1 bansari  2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b ,diff;
+    printf ("enter two number");
+    scanf("%d,%d" , &a,&b);
+    sum=a-b;
+    printf("different= %d\n",sum0)
+    return 0 ;
+
+}
+

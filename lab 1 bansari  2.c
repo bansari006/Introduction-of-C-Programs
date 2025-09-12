d0c704538b61c4b212f240f7409142af74e9384b
bansari006 <bansarigajera10@gmail.com>
2025-09-12 10:25:20 +0530

    Update lab 1 bansari  2.c

--- lab 1 bansari  2.c
+++ lab 1 bansari  2.c
@@ -4,9 +4,10 @@ int main()
     int a,b ,diff;
     printf ("enter two number");
     scanf("%d,%d" , &a,&b);
-    sum=a-b;
+    diff=a-b;
     printf("different= %d\n",sum0)
     return 0 ;
 
 }
+
 

624452473d9eed9d4c952c43a35b894c9b82a67c
bansari006 <bansarigajera10@gmail.com>
2025-09-19 09:06:40 +0530

    Add files via upload

---
+++ LAB 2 BANSARI 21.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int n, r;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    while(n > 0) {#include <stdio.h>
+int main() {
+    int n, r;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    while(n > 0) {
+        r = n % 10;
+        printf("%d ", r);
+        n = n / 10;
+    }
+    return 0;
+}
+
+        r = n % 10;
+        printf("%d ", r);
+        n = n / 10;
+    }
+    return 0;
+}
+

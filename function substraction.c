86a13b9ff6525a83818f269c7a9976540945babb
bansari006 <bansarigajera10@gmail.com>
2025-10-10 10:16:20 +0530

    Add files via upload

---
+++ function substraction.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void sub()
+
+{
+ int a,b ,product=0;
+ printf("enter two number");
+ scanf("%d%d",&a,&b);
+ product=a-b;
+ printf("\n product is= %d",product);
+}
+
+ int main()
+ {
+     sub();
+     sub();
+
+ }
+

7f083463a470fb989b809a212a56e57e85612701
bansari006 <bansarigajera10@gmail.com>
2025-10-10 10:10:13 +0530

    Add files via upload

---
+++ functions addition.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void add()
+
+{
+ int a,b ,sum=0;
+ printf("enter two number");
+ scanf("%d%d",&a,&b);
+ sum=a+b;
+ printf("\n sum is= %d",sum);
+}
+
+ int main()
+ {
+     add();
+     add();
+
+ }
+

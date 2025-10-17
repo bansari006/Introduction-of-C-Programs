8698a987aef5a7222c7b6c58f7e57a2e7a782ea7
bansari006 <bansarigajera10@gmail.com>
2025-10-17 10:12:54 +0530

    Add files via upload

---
+++ Function.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int power (int a, int b)
+{
+for (int i=1;i<=b;i++){
+     result = result*a;
+{
+    return result;
+
+}
+int main () {
+    int a,b
+    printf("enter base=(a)");
+    scanf("%d,&a");
+    printf("enter exponent b");
+    scanf("%b,&b");
+    printf ("%d^%d=%d\n",(a,b)
+    power(a,b));
+    return 0;
+}
+
+
